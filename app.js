d3b198f91b80eb299d14596b8e155d0381c37f20
Tubagus Adhitya Permana <aditt7116@gmail.com>
2023-11-14 13:48:42 +0700

    update app.js

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require('express')
 const mysql = require('mysql');
-const App = express()
-App.disable("x-powered-by");
+const app = express()
+app.disable("x-powered-by");
 const path = require('path')
 require('dotenv').config();
 
 // Import Middleware
 const logger = require('./middleware/logger')
-App.use(logger)
+app.use(logger)
 const connection = require('./middleware/db_connect');
 
 // Dashboard
-App.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-App.get('/app1', (req, res) => {
+app.get('/app1', (req, res) => {
   res.send('Hello this Apps 1!')
 });
 
-App.get('/app2', (req, res) => {
+app.get('/app2', (req, res) => {
   res.send('Hello this App 2!')
 });
 
-App.get('/users', (req, res, next) => {
+app.get('/users', (req, res, next) => {
   const sql = "SELECT * FROM tb_data ORDER BY id desc"
   connection.query(sql,(error, fields) => {
     if (error) {
@@ -32,8 +32,8 @@ App.get('/users', (req, res, next) => {
   })
 });
 
-App.listen(process.env.APP_PORT, () => {
+app.listen(process.env.APP_PORT, () => {
   console.log(`Example app listening on port ${process.env.APP_PORT}`)
 })
 
-module.exports = App
+module.exports = app
